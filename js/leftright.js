4484f3f6479e0f74508d26f6402a3ddc342d3c7a
aplac <oliverstreiter@gmail.com>
2023-12-29 21:37:04 +0800

    javascript added

---
+++ js/leftright.js
@@ -0,0 +1,27 @@
+let touchstartX = 0
+let touchendX = 0
+let touchstartY = 0
+let touchendY = 0
+
+function checkLeftRight(lurl,rurl) {
+  if ((Math.abs(touchendX - touchstartX)) 
+      > (Math.abs(touchendY - touchstartY))) {
+      if (touchendX+65 
+          < touchstartX && '$lurl' !== '') {window.location.href=lurl }
+      if (touchendX 
+          > touchstartX+65 && '$rurl' !== '') {window.location.href=rurl }
+  }
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX;
+  touchstartY = e.changedTouches[0].screenY;
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX;
+  touchendY = e.changedTouches[0].screenY;
+  checkDirection();
+})
+
+
